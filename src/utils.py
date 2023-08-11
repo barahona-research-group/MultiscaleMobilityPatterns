59ae7be0018193e4d3ab8731a0ab108812fe455f
d-schindler <60650591+d-schindler@users.noreply.github.com>
2023-08-11 11:30:03 +0200

    improve notebooks

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+from sklearn import metrics
 from sklearn.linear_model import LinearRegression
 
 
@@ -14,3 +17,34 @@ def R2_score(X, y):
 
     # Coefficient of determination
     return LsLin.score(X, y)
+
+
+def entropy(labels):
+    """Calculates the Entropy for a labeling.
+    Parameters
+    ----------
+    labels : int array, shape = [n_samples]
+        The labels
+    Notes
+    -----
+    The logarithm used is the natural logarithm (base-e).
+    """
+    if len(labels) == 0:
+        return 1.0
+    label_idx = np.unique(labels, return_inverse=True)[1]
+    pi = np.bincount(label_idx).astype(np.float64)
+    pi = pi[pi > 0]
+    pi_sum = np.sum(pi)
+    return -np.sum((pi / pi_sum) * (np.log(pi) - np.log(pi_sum)))
+
+
+def variation_of_information(x, y, normalised=True):
+    """Calculates the (Normalised) Variation of Information for two partitions."""
+    Ex = entropy(x)
+    Ey = entropy(y)
+    I = metrics.mutual_info_score(x, y)
+
+    if normalised:
+        return (Ex + Ey - 2 * I) / (Ex + Ey - I)
+    else:
+        return Ex + Ey - 2 * I
